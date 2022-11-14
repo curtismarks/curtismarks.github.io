3193f598ff73c9eeb240aad3581fc5befea6d5c7
curtismarks <curtismarks25@gmail.com>
2022-11-14 18:20:25 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,10 +10,11 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  var positionX = 0; // the x-coordinate location for the box
-  var speedX = 5; // the speed for the box along the x-axis
-  var positionY = 0; // the y-coordinate location for the box
-  var speedY = 5; // the speed for the box along the y-axis
+  var BOARD_WIDTH = $("#board").width();
+  var BOARD_HEIGHT = $("#board").height();
+  var WALKER_HEIGHT = $("#walker").height();
+  var WALKER_WIDTH = $("#walker").width();
+
   // Game Item Objects
   var KEY ={
     "UP": 38,
@@ -24,9 +25,12 @@ function runProgram(){
   //  one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on("keydown", handleKeyDown);    // change 'eventType' to the type of event you want to handle
-  $(document).on("keyup", handleKeyDown);    // change 'eventType' to the type of event you want to handle
-  $(document).on("keyleft", handleKeyDown);    // change 'eventType' to the type of event you want to handle
-  $(document).on("keyright", handleKeyDown);    // change 'eventType' to the type of event you want to handle
+  $(document).on("keyup", handleKeyUp);    // change 'eventType' to the type of event you want to handle
+  //other variables
+  var positionX = 0; // the x-coordinate location for the box
+  var speedX = 0; // the speed for the box along the x-axis
+  var positionY = 0; // the y-coordinate location for the box
+  var speedY = 0; // the speed for the box along the y-axis
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
@@ -37,7 +41,7 @@ function runProgram(){
   function newFrame() {
     repositionGameItem();
     redrawGameItem();
-
+    stopWalker();
   }
   
   /* 
@@ -61,6 +65,14 @@ function runProgram(){
       console.log("You've moved left. YAY!!!");
     }
   }
+  function handleKeyUp(event){
+    if(event.which === KEY.DOWN || event.which === KEY.UP){
+      speedY = 0;
+    }
+    if(event.which === KEY.RIGHT || event.which === KEY.LEFT){
+      speedX = 0;
+    }
+  }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -75,7 +87,22 @@ function runProgram(){
     $("#walker").css("left", positionX);
     
   }
-  
+
+  function stopWalker(){
+    if(positionX > BOARD_WIDTH - WALKER_WIDTH){
+      positionX = BOARD_WIDTH - WALKER_WIDTH;
+    }
+    if(positionY > BOARD_HEIGHT - WALKER_HEIGHT){
+      positionY = BOARD_HEIGHT - WALKER_HEIGHT;
+    }
+    if(positionY < 0){
+      positionY = 0;
+    }
+    if(positionX < 0){
+      positionX = 0;
+    }
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
