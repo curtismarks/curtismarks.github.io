67654ebb2d28de287c6e36aa178b6cb7eea217fc
curtismarks <curtismarks25@gmail.com>
2023-02-27 17:39:37 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -11,12 +11,39 @@ function runProgram(){
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
-  // Game Item Objects
+  const BOARD_WIDTH = $('#board').width();
+  const BOARD_HEIGHT = $('#board').height();
 
+  var KEY ={
+    "UP": 38,
+    "DOWN": 40,
+    "W":87,
+    "S":83,
+  }
+  // Game Item Objects
+  function GameItem (x,y,speedX,speedY,id){
+    var item={
+      x: x,
+      y: y,
+      speedX: speedX,
+      speedY: speedY,
+      h: $(id).height(),
+      w: $(id).width(),
+      id: id,
+    }
+    return item;
+  }
+
+  
+
+  var player1 = GameItem(10, 200, 0, 0, '#player1');
+  var player2 = GameItem(BOARD_WIDTH - 10 - $('#player2').width(), 200, 0, 0, '#player2');
+  var ball = GameItem(BOARD_WIDTH / 2, BOARD_HEIGHT / 2,(Math.random() > 0.5 ? -3 : 3), (Math.random() > 0.5 ? -3 : 3),'#ball');
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,21 +54,74 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    drawItem(player1);
+    drawItem(ball);
+    drawItem(player2);
+    UpdateItem(player1);
+    UpdateItem(player2);
+    UpdateItem(ball);
+    stopBall();
+    stopPlayers();
+    wallCollsion(ball);
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if(event.which === KEY.S){
+      player1.speedY = 5;
+      console.log("You've moved down. YAY!!!");
+    }
+    if(event.which === KEY.W){
+      player1.speedY = -5;
+      console.log("You've moved up. YAY!!!");
+    }
+  
+    if (event.which === KEY.UP) {
+      player2.speedY = -5;
+      console.log("W pressed");
+    }
+    if (event.which === KEY.DOWN) {
+      player2.speedY = 5;
+      console.log("s pressed");
+    }
+  }
+  function handleKeyUp(event) {
+    if(event.which === KEY.DOWN || event.which === KEY.UP || event.which === KEY.W || event.which === KEY.S){
+      player1.speedY = 0;
+      player2.speedY = 0;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  function drawItem(obj){
+    $(obj.id).css("top", obj.y);
+    $(obj.id).css("left", obj.x);
+  }
 
+  function wallCollsion(obj){
+    return obj < 0 ? 0 : obj > 255 ? 255 : obj;
+  }
+
+  function doCollide(obj1, obj2) {
+    return obj1 - obj2;
+  }
+  
+  if (doCollide(ball, player1)) {
+    ball.speedY = 5;
+  }
+
+  if (doCollide(ball, player2)) {
+    ball.speedY = -5;
+  }
+
+  function UpdateItem(obj){
+    obj.x = obj.x + obj.speedX;
+    obj.y = obj.y + obj.speedY;
+  }
   
   function endGame() {
     // stop the interval timer
@@ -50,5 +130,5 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+
 }
